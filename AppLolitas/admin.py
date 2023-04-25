91366c79ec90437547e4959a609735d88f9fa95a
RodrigoBuda <rodrigo.buda@outlook.com>
2023-04-25 14:02:03 -0300

    a

--- AppLolitas/admin.py
+++ AppLolitas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuaria
 
 # Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created",)
+
+
+admin.site.register(Usuaria, TaskAdmin)
